c2af7479ec3d96d7690b80009aec5cebc07cb9e1
sinceseptember4 <sinceseptember4@gmail.com>
2021-11-16 13:19:35 +0900

    delete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import React, {useEffect, useState} from "react";
 function App() {
+  const [name, setName] = useState('')
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => setUsers(response.data))
+      .catch(error => console.log(error))
+  }, [])
+
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+  
+  const createNewUser = () => {
+    axios.post('https://jsonplaceholder.typicode.com/users', {
+      name: name
+    })
+    .then(response => {
+      setUsers([...users, response.data])
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  const deleteUser = (id) => {
+    axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+    .then(response => {
+      console.log(response)
+      setUsers(users.filter(user => user.id !== id))
+    })
+    .catch(error => console.log(error))
+  }
+
+  
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <input value={name} onChange={handleChange} /><br />
+    <button onClick={createNewUser}>作成</button>
+      <ul>
+    {users.map((user, index) => (
+      <li key={index}>{user.name}:
+        <button onClick={() => deleteUser(user.id)}>削除</button>
+      </li>
+    ))}
+  </ul>
+  </div>
+  )
 }
 
 export default App;
